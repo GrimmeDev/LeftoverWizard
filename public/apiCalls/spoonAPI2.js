44c71f1a88b200e31e9a656e40aaa265f54cd8a7
Umut Alptekin <amacalptekin@gmail.com>
2020-11-04 21:53:45 +0300

    turned 2nd api call to async function & make it look like the 1st api call

--- public/apiCalls/spoonAPI2.js
+++ public/apiCalls/spoonAPI2.js
@@ -2,24 +2,21 @@
 
 //example api call for getting info on a specific recipe with ID: 
 // https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
-function SearchbyRecipeID(recipeID, API) {
-  // var recipeID = 
-  const nutValues = false;
+async function SearchbyRecipeID(recipeID, API) {
+  const nutValues = false; 
   let queryURL = `https://api.spoonacular.com/recipes/${recipeID}/information?includeNutrition=${nutValues}&apiKey=${API}`;
   // var queryURL = "https://api.spoonacular.com/recipes/" + recipeID + "/information?includeNutrition="+ nutValues+ "&apiKey=" + API;
-  $.ajax({ // Performing an AJAX request with the queryURL
+  return await $.ajax({ // Performing an AJAX request with the queryURL
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-      console.log(response);
-      const recipeTitle = response.title;
-      const recipeImage = response.image;
-      const recipeSourceLink = response.sourceUrl;
-      const spoonacularURL = response.spoonacularSourceUrl;
-      const readyTime = response.readyInMinutes;
-          
-      
-
   })
+  // .then(function(response) {
+  //     console.log(response);
+  //     const recipeTitle = response.title;
+  //     const recipeImage = response.image;
+  //     const recipeSourceLink = response.sourceUrl;
+  //     const spoonacularURL = response.spoonacularSourceUrl;
+  //     const readyTime = response.readyInMinutes;     
+  //})
 }
 module.exports = SearchbyRecipeID;
