afd89587c14ada76f7b1e81757f2d0cfdfc9f87e
Delmy Arguello <kroarguelloadel@gmail.com>
2020-11-05 14:31:41 -0600

    getting last style

--- public/js/viewRecipes.js
+++ public/js/viewRecipes.js
@@ -5,12 +5,12 @@ $(document).ready(
         // recipes[i].name and recipes[i].link are the info we need
         let list = ``;
         for (let i = 0; i < recipes.length; i++) {
-            list += `<li><p>Name: ${recipes[i].name} </p><p> Link: ${recipes[i].link}</p></li>`;
+            list += `<div class="grid-item"><p>Name: ${recipes[i].name} </p><p> Link: ${recipes[i].link}</p></div>`;
         }
         var userRecipes = `
-                <ol>
+                <div class="row grid-container">
                     ${list}
-                </ol>
+                </div>
             `;
         $(".card-deck").append(userRecipes);
     })
